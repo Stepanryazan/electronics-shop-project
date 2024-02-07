b5202f364d7e2d9cc598cf51910d5ba16487d039
Stepan <offish.sk@icloud.com>
2024-02-08 01:11:07 +0300

    dz_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
+
+    @property
+    def name(self):
+        return f"{self.__name}"
+
+    @name.setter
+    def name(self, value):
+        if len(value) <= 10:
+            self.__name = value
+            print(f'Корректное название - {value}')
+        else:
+            self.__name = value[:10]
+            print(f'Длинное слово - {value[:10]}')
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +41,32 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        all_price = self.quantity * self.price
+        return all_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price - self.price * self.pay_rate
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @classmethod
+    def instantiate_from_csv(cls, file_name):
+        cls.all.clear()
+        with open(file_name, 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+            for item in items:
+                name = item['name']
+                price = cls.string_to_number(item['price'])
+                quantity = int(item['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(number):
+        return int(float(number))
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,49 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def position():
+    return Item("Смартфон", 100, 1)
+
+
+def test_item_init(position):
+    assert position.name == "Смартфон"
+    assert position.price == 100
+    assert position.quantity == 1
+
+
+def test_calculate_total_price():
+    item = Item('Смартфон', 20000, 100)
+    assert item.calculate_total_price() == 2000000
+
+
+def test_apply_discount():
+    item = Item('Смартфон', 2000, 3)
+    Item.pay_rate = 0.1
+
+    item.apply_discount()
+
+    assert item.price == 1800.0
+
+
+def test_name_setter():
+    item = Item('Телефон', 10000, 5)
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+
+def test_name_setter_truncate():
+    item = Item('Телефон', 25000, 3)
+    item.name = 'Суперсмартфон'
+    assert item.name == 'Суперсмартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+
